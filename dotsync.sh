22091fd3197c5c59f90ad704c9f8abea89ab1014
Herman Bonnes <h.bonnes@gmail.com>
2018-05-05 01:23:45 +0200

    Fixed dotsync.sh

--- dotsync.sh
+++ dotsync.sh
@@ -22,7 +22,7 @@ function first-time {
 
     # Dependencies
     sudo apt update
-    sudo apt upgrade
+    sudo apt upgrade -y
     sudo apt-get install -y libncurses5-dev libgnome2-dev libgnomeui-dev \
     libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
     libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
