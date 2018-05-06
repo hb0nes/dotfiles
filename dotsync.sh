991e43d191fe03d57472edb14c6db430d068805f
Herman Bonnes <h.bonnes@gmail.com>
2018-05-06 10:02:15 +0200

    added some files

--- dotsync.sh
+++ dotsync.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/files"
-if [[ $1 != "pull" && $1 != "push" && $1 != "first-time" && $1 != "debug" ]]; then
-    echo "Usage: install.sh <pull/push/first-time>"
-    echo "Use first-time when you haven't even installed zsh/oh-my-zsh yet."
+if [[ $1 != "-pull" && $1 != "-push" && $1 != "-first-time" && $1 != "-debug" ]]; then
+    echo "Usage: install.sh -pull, -push, -first-time, -debug"
+    echo
+    echo "Use -first-time when you want to install everything on a new machine."
+    echo "Use -first-time with --force when you want to overwrite previous attempts."
+    echo "Use pull when you want to sync the dotfiles with the repo."
+    echo "Use pull when you made local changes and want to push them to the repo."
     exit
 fi
 
@@ -12,7 +16,7 @@ function debug {
 
 function first-time {
     sudo git fetch > /dev/null
-    sudo git pull > /dev/null 
+    sudo git pull > /dev/null
 
     for file in $(cat $DIR/../filelist); do
         cp -r $DIR/$file ~
@@ -24,56 +28,47 @@ function first-time {
     # Dependencies
     sudo apt update
     sudo apt-get install -y libncurses5-dev libgnome2-dev libgnomeui-dev \
-    libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
-    libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
-    python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev checkinstall build-essential cmake --fix-missing > /dev/null
+        libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
+        libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
+        python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev checkinstall build-essential cmake --fix-missing > /dev/null
     #Install tmux, tmuxinator, zsh, vim80 with youcompleteme plugin
-    if [[ ! -f /usr/bin/tmux ]]; then
+    if [[ $2 == '--force' ]]; then
         sudo apt-get install -y tmux > /dev/null
-    else
-        echo "Already have tmux installed."
-    fi
-    if [[ ! -f /usr/local/bin/tmuxinator ]]; then
         sudo gem install tmuxinator
-    else
-        echo "Already have Tmuxinator installed."
-    fi
-    if [[ ! -f /bin/zsh5 ]]; then
         sudo apt-get install -y zsh > /dev/null
-    else
-        echo "Already have ZSH installed."
-    fi
-    if [[ ! -d /usr/local/share/vim/vim80 ]]; then
         installVim
+        cd ~/.vim/bundle/YouCompleteMe
+        sudo ./install.py --clang-completer
+        sleep 2
+        bash
     else
-        echo "Already have ViM80 installed."
+        if [[ ! -f /usr/bin/tmux ]]; then
+            sudo apt-get install -y tmux > /dev/null
+        else
+            echo "Already have tmux installed."
+        fi
+        if [[ ! -f /usr/local/bin/tmuxinator ]]; then
+            sudo gem install tmuxinator
+        else
+            echo "Already have Tmuxinator installed."
+        fi
+        if [[ ! -f /bin/zsh5 ]]; then
+            sudo apt-get install -y zsh > /dev/null
+        else
+            echo "Already have ZSH installed."
+        fi
+        if [[ ! -d /usr/local/share/vim/vim80 ]]; then
+            installVim
+        else
+            echo "Already have ViM80 installed."
+        fi
+        #Make YouCompleteMe with Python3.5
+        cd ~/.vim/bundle/YouCompleteMe
+        sudo ./install.py --clang-completer
+        sleep 2
+        bash
     fi
-
-    #Make YouCompleteMe with Python3.5
-    cd ~/.vim/bundle/YouCompleteMe    
-    sudo ./install.py --clang-completer
-    sleep 3
-    bash
-    #echo -e "The following wizard will ask you to install stuff in this order: \n1.TMUX\n2.ZSH\n3.ViM8"
-    #Install tmux
-    #read -p "Do you want to install TMUX?" -r -n 1
-    #echo
-    #if [[ $REPLY =~ ^[YyJj]$ ]]; then
-        #sudo apt-get install -y tmux 1>/dev/null
-    #fi
-    #Install ZSH
-    #read -p "Do you want to install ZSH?" -r -n 1
-    #echo
-    #if [[ $REPLY =~ ^[YyJj]$ ]]; then
-        #sudo apt-get install -y zsh 1>/dev/null
-    #fi
-    #Install ViM8 
-    #read -p "Do you want to install ViM 8 with Python3.5 and YouCompleteMe (autocompletion) support? Please note this will take a long time on most systems." -r -n 1
-    #echo
-    #if [[ $REPLY =~ ^[YyJj]$ ]]; then
-        #installVim
-    #fi
-} 
+}
 
 function pull {
     sudo git fetch > /dev/null 2>&1
@@ -98,7 +93,7 @@ function pull {
 }
 
 function push {
-    read -p "Push all dotfiles?" -n 1 ans 
+    read -p "Push all dotfiles?" -n 1 ans
     echo
     for file in $(cat filelist); do
         if [[ $ans =~ ^[YyJj]$ ]]; then
@@ -136,18 +131,18 @@ function installVim {
     git clone https://github.com/vim/vim.git
     cd vim
     sudo ./configure --with-features=huge \
-                                --enable-multibyte \
-                                --enable-rubyinterp=yes \
-                                --enable-python3interp=yes \
-                                --with-python3-config-dir=$PYTHONCONFIGDIR
-                                --enable-perlinterp=yes \
-                                --enable-luainterp=yes \
-                                --enable-gui=gtk2 \
-                                --enable-cscope \
-                                --prefix=/usr/local
+        --enable-multibyte \
+        --enable-rubyinterp=yes \
+        --enable-python3interp=yes \
+        --with-python3-config-dir=$PYTHONCONFIGDIR
+    --enable-perlinterp=yes \
+        --enable-luainterp=yes \
+        --enable-gui=gtk2 \
+        --enable-cscope \
+        --prefix=/usr/local
     make VIMRUNTIMEDIR=/usr/local/share/vim/vim80
     #Install that shit
-    echo -e "\n\n\n\n\n" | sudo checkinstall 
+    echo -e "\n\n\n\n\n" | sudo checkinstall
     # Cleanup
     sudo rm -rf ~/vim
     # make sure ZSH starts even on Win10 WSL
@@ -155,18 +150,18 @@ function installVim {
     echo "All done!"
 }
 
-if [[ $1 == 'push' ]]; then
+if [[ $1 == '-push' ]]; then
     push
 fi
 
-if [[ $1 == 'pull' ]]; then
+if [[ $1 == '-pull' ]]; then
     pull
 fi
 
-if [[ $1 == 'first-time' ]]; then
-    first-time 
+if [[ $1 == '-first-time' ]]; then
+    first-time
 fi
 
-if [[ $1 == 'debug' ]]; then
+if [[ $1 == '-debug' ]]; then
     debug
 fi
