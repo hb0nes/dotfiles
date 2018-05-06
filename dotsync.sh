ef039b3dcbfe469c64172b1d7b65949ad8424315
root <root@DESKTOP-E6GCLNI.localdomain>
2018-05-06 16:52:00 +0200

    Final touches on dotsync.sh

--- dotsync.sh
+++ dotsync.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/files"
-if [[ $1 != "-pull" && $1 != "-push" && $1 != "-first-time" && $1 != "-debug" ]]; then
-    echo "Usage: install.sh -pull, -push, -first-time, -debug"
-    echo
-    echo "Use -first-time when you want to install everything on a new machine."
-    echo "Use -first-time with --force when you want to overwrite previous attempts."
-    echo "Use pull when you want to sync the dotfiles with the repo."
-    echo "Use pull when you made local changes and want to push them to the repo."
-    exit
-fi
+function usage() {
+    echo "Usage: $0 [-h --help] [-d --download] [-u --upload] [-f --first-time <force>] [-t --test] 1>&2"
+    exit 1
+}
 
-function debug {
+function help() {
+    echo "-f or --first-time when you want to install everything on a new machine."
+    echo "-f force or --first-time force when you want to overwrite previous attempts."
+    echo "-d or --download when you want to sync the dotfiles with the repo."
+    echo "-u or --upload when you made local changes and want to push them to the repo."
+    exit 1
+}
+
+function testRun() {
+    echo $@
     echo $DIR
 }
 
-function first-time {
+function first-time() {
+    if [[ $force == "force" ]]; then
+        echo 'Forced install.'
+    fi
+
     sudo git fetch > /dev/null
     sudo git pull > /dev/null
 
     for file in $(cat $DIR/../filelist); do
-        cp -r $DIR/$file ~
+        sudo cp -r $DIR/$file $HOME
         sudo cp -r $DIR/$file /root/
-        echo "Copied $DIR/$file to home (~)"
+        echo "Copied $DIR/$file to home ($HOME)"
     done
     mkdir -p $HOME/school
-    echo "Done copying files to your home (~)."
+    echo "Done copying files to your home ($HOME)."
 
     # Dependencies
     sudo apt update
@@ -32,46 +40,36 @@ function first-time {
         libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
         libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
         python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev checkinstall build-essential cmake --fix-missing > /dev/null
+
     #Install tmux, tmuxinator, zsh, vim80 with youcompleteme plugin
-    if [[ $2 == '--force' ]]; then
+    if [[ ! -f /usr/bin/tmux || $force == 'force' ]]; then
         sudo apt-get install -y tmux > /dev/null
-        sudo gem install tmuxinator
-        sudo apt-get install -y zsh > /dev/null
-        installVim
-        sudo ~/.vim/bundle/YouCompleteMe/install.py --clang-completer
-        sudo /root/.vim/bundle/YouCompleteMe/install.py --clang-completer
-        sleep 2
-        bash
     else
-        if [[ ! -f /usr/bin/tmux ]]; then
-            sudo apt-get install -y tmux > /dev/null
-        else
-            echo "Already have tmux installed."
-        fi
-        if [[ ! -f /usr/local/bin/tmuxinator ]]; then
-            sudo gem install tmuxinator
-        else
-            echo "Already have Tmuxinator installed."
-        fi
-        if [[ ! -f /bin/zsh5 ]]; then
-            sudo apt-get install -y zsh > /dev/null
-        else
-            echo "Already have ZSH installed."
-        fi
-        if [[ ! -d /usr/local/share/vim/vim80 ]]; then
-            installVim
-        else
-            echo "Already have ViM80 installed."
-        fi
-        #Make YouCompleteMe with Python3.5
-        sudo ~/.vim/bundle/YouCompleteMe/install.py --clang-completer
-        sudo /root/.vim/bundle/YouCompleteMe/install.py --clang-completer
-        sleep 2
-        echo "All done! Please start your terminal."
+        echo "Already have tmux installed."
     fi
+    if [[ ! -f /usr/local/bin/tmuxinator || $force == 'force' ]]; then
+        sudo gem install tmuxinator
+    else
+        echo "Already have Tmuxinator installed."
+    fi
+    if [[ ! -f /bin/zsh5 || $force == 'force' ]]; then
+        sudo apt-get install -y zsh > /dev/null
+    else
+        echo "Already have ZSH installed."
+    fi
+    if [[ ! -d /usr/local/share/vim/vim80 || $force == 'force' ]]; then
+        installVim
+    else
+        echo "Already have ViM80 installed."
+    fi
+    #Make YouCompleteMe with Python3.5
+    sudo $HOME/.vim/bundle/YouCompleteMe/install.py --clang-completer
+    sudo /root/.vim/bundle/YouCompleteMe/install.py --clang-completer
+    sleep 2
+    echo "All done! Please restart your terminal."
 }
 
-function pull {
+function download() {
     sudo git fetch > /dev/null 2>&1
     echo "Fetching."
     sudo git pull > /dev/null 2>&1
@@ -80,32 +78,32 @@ function pull {
     echo
     for file in $(cat filelist); do
         if [[ $ans =~ ^[YyJj]$ ]]; then
-            cp -r $DIR/$file ~
+            cp -r $DIR/$file $HOME
             sudo cp -r $DIR/$file /root/
-            echo "Copied $DIR/$file to home (~)"
+            echo "Copied $DIR/$file to home ($HOME)"
         else
             read -p "Pull $DIR/$file?" -n 1 -r
             echo
             if [[ $REPLY =~ ^[YyJj]$ ]]; then
-                sudo cp -r $DIR/$file ~
+                sudo cp -r $DIR/$file $HOME
             fi
         fi
     done
     zsh
 }
 
-function push {
+function upload() {
     read -p "Push all dotfiles?" -n 1 ans
     echo
     for file in $(cat filelist); do
         if [[ $ans =~ ^[YyJj]$ ]]; then
-            sudo cp -r ~/$file $DIR
+            sudo cp -r $HOME/$file $DIR
             echo "Copied $file to $DIR"
         else
-            read -p "Push ~/$file?" -n 1 -r
+            read -p "Push $HOME/$file?" -n 1 -r
             echo
             if [[ $REPLY =~ ^[YyJj]$ ]]; then
-                sudo cp -r ~/$file $DIR
+                sudo cp -r $HOME/$file $DIR
             fi
         fi
     done
@@ -118,7 +116,7 @@ function push {
     echo "Pushed."
 }
 
-function installVim {
+function installVim() {
     # install dependencies
     PYTHONCONFIGDIR=$(ls /usr/lib/python3.5/ | grep -i config | grep -v ".py")
     echo "Python 3.5 config dir: ${PYTHONCONFIGDIR}"
@@ -128,7 +126,7 @@ function installVim {
     #Delete current vim
     sudo apt remove -y vim vim-runtime gvim
     #Clone vim repo, configure and make
-    cd ~
+    cd $HOME
     sudo rm -rf vim
     git clone https://github.com/vim/vim.git
     cd vim
@@ -146,24 +144,39 @@ function installVim {
     #Install that shit
     echo -e "\n\n\n\n\n" | sudo checkinstall
     # Cleanup
-    sudo rm -rf ~/vim
+    sudo rm -rf $HOME/vim
     # make sure ZSH starts even on Win10 WSL
-    echo "zsh" >> ~/.bashrc
+    echo "zsh" >> $HOME/.bashrc
     echo "All done!"
 }
 
-if [[ $1 == '-push' ]]; then
-    push
-fi
+while [[ $# > 0 ]]; do
+    key=$1
+    case $key in
+        -d|--download)
+            download
+            shift # key
+            ;;
+        -u|--upload)
+            upload
+            shift # key
+            ;;
+        -f|--first-time)
+            force=$2
+            first-time $force
+            shift # key
+            shift # value
+            ;;
+        -t|--test)
+            testRun $@
+            shift $# # key
+            ;;
+        -h|--help)
+            help
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
 
-if [[ $1 == '-pull' ]]; then
-    pull
-fi
-
-if [[ $1 == '-first-time' ]]; then
-    first-time
-fi
-
-if [[ $1 == '-debug' ]]; then
-    debug
-fi
--- files/.vimrc
+++ files/.vimrc
@@ -17,3 +17,4 @@ filetype plugin indent on
 set tabstop=4
 set shiftwidth=4
 set expandtab
+cmap w!! w !sudo tee > /dev/null %
