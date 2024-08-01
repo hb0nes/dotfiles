5907dd07faadd7450e1271972f9dd088c3a676d7
Herman Bonnes <herman@sentillia.com>
2024-08-01 12:26:52 +0200

    Update clipboard

--- .config/nvim/lua/opts.lua
+++ .config/nvim/lua/opts.lua
@@ -12,7 +12,7 @@ vim.o.shiftwidth = 2
 vim.o.expandtab = true
 
 -- Copy to clipboard on yank
-vim.o.clipboard = "unnamedplus"
+vim.o.clipboard = "unnamed"
 
 -- Write vim swap file sooner, also helps with git diff tracking
 vim.o.updatetime = 100
